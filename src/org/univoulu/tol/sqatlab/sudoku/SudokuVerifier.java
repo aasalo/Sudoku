dbdc24abb726415ad50ae1601f268af78b3049e4
somename <someemail>
2015-10-02 13:00:10 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,10 +1,20 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
+import java.util.Arrays;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class SudokuVerifier {
  
+	char[] validRow = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	
 	public int verify(String candidateSolution) {
 		
-		return 0;
+		char[] firstRow = candidateSolution.substring(0, 8).toCharArray();
+		Arrays.sort(firstRow);
+		
+		if(!Arrays.equals(firstRow, validRow)){
+			return -3;
+		}else return 0;
 	}
 }
