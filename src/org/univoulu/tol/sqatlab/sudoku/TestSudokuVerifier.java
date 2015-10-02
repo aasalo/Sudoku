c679457759940c3621de2d6132e5fe64979427c5
somename <someemail>
2015-10-02 12:51:34 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,13 +8,12 @@ import org.junit.runners.Parameterized.Parameter;
 public class TestSudokuVerifier {
 
 	SudokuVerifier verifier = new SudokuVerifier();
-	String correctRow = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	String incorrectRow = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-	
+
 	@Test
 	public void testVerifier() {
-		String firstRow = correctRow.substring(0, 8);
-		verifier.verify(firstRow);
+		String firstRow = "111111111222222222333333333444444444555555555666666666777777777888888888999999999";
+		int result = verifier.verify(firstRow);
+		assertEquals(-3, result);
 		
 	}
 
