112176bc35ad59b867c52cc48782b53836db3df2
somename <someemail>
2015-10-02 13:22:22 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -10,25 +10,25 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		
-		char[] firstRow = candidateSolution.substring(0, 9).toCharArray();
-		Arrays.sort(firstRow);
-		
-		char[] secondRow = candidateSolution.substring(9, 18).toCharArray();
-		Arrays.sort(secondRow);
-		
-		for(int i = 0; i <= 72; i = i+9){
-			if(checkIfRowContainsDuplicates(candidateSolution.substring(i, i+9).toCharArray())){
-
-				return -3;
-			}
+		if(checkAllRows()){
+			return -3;
 		}
-		return 0;
+		else return 0;
 	}
-	
 
 	private boolean checkIfRowContainsDuplicates(char[] row) {
 		char[] validRow = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 		
 		return !Arrays.equals(row, validRow);
+	}
+	
+	private boolean checkAllRows() {
+		for(int i = 0; i <= 72; i = i+9){
+		if(checkIfRowContainsDuplicates(candidateSolution.substring(i, i+9).toCharArray())){
+
+			return false;
+		}
+	}
+	return true;
 	}
 }
